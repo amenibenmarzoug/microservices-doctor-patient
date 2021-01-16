5d5b720e0bf2d93ab364af17cc8428769cc47176
Eya-Swayah <eyaswayah000@gmail.com>
2021-01-16 22:56:39 +0100

    third commit

--- gateway-service/src/main/java/com/eniso/pm/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/eniso/pm/gatewayservice/GatewayServiceApplication.java
@@ -7,11 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.codec.ServerCodecConfigurer;
-
-
-
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication extends SpringBootServletInitializer {
 	DataSource dataSource ;
