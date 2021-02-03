e832b5e0587fa9a163734e0f5bd9ea80c9e4646c
amenibenmarzoug <benmarzoug.ameni@gmail.com>
2021-02-03 03:04:49 +0100

    docker&mongo

--- discoveryserver/src/main/java/com/eniso/pm/discoveryserver/DiscoveryserverApplication.java
+++ discoveryserver/src/main/java/com/eniso/pm/discoveryserver/DiscoveryserverApplication.java
@@ -4,12 +4,13 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableEurekaServer
 
 
--- service-patient/src/main/java/com/eniso/pm/servicepatient/repository/PatientRepository.java
+++ service-patient/src/main/java/com/eniso/pm/servicepatient/repository/PatientRepository.java
@@ -1,12 +1,12 @@
 package com.eniso.pm.servicepatient.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eniso.pm.servicepatient.entity.Patient;
 
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends MongoRepository<Patient, Long> {
 
 }
