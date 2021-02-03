80e949eb75a88e052f562018d267e421fa1cd245
Eya-Swayah <eyaswayah000@gmail.com>
2021-02-03 13:47:09 +0100

    third commit

--- service-doctor/src/main/java/com/eniso/pm/servicedoctor/controller/DoctorController.java
+++ service-doctor/src/main/java/com/eniso/pm/servicedoctor/controller/DoctorController.java
@@ -1,10 +1,17 @@
 package com.eniso.pm.servicedoctor.controller;
 
+
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 
 //@EnableJpaRepositories 
 
@@ -24,6 +31,7 @@ import com.eniso.pm.servicedoctor.entities.Doctor;
 import com.eniso.pm.servicedoctor.repository.DoctorRepository;
 import com.eniso.pm.servicedoctor.service.DoctorService;
 
+
 //@CrossOrigin("*")
 @CrossOrigin(origins = "http://localhost:4200")
 
@@ -31,64 +39,79 @@ import com.eniso.pm.servicedoctor.service.DoctorService;
 @ComponentScan(basePackageClasses = DoctorService.class)
 @RequestMapping(value = "/api")
 public class DoctorController {
-	
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	@Autowired
 	DoctorRepository doctorRepository;
 	@Autowired
-	
+
 	DoctorService doctorService;
-	
+
 	@Autowired
 	public DoctorController(DoctorService doctorService) {
 		this.doctorService = doctorService;
-	} 
-
-	@GetMapping("/doctors") 
-	public List<Doctor> findAll() {
-	
-		return doctorService.findAll();
-		
 	}
-	
+	 @RequestMapping(value = "/template/patients")
+	   public String getPatiens() {
+	      HttpHeaders headers = new HttpHeaders();
+	      headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	      HttpEntity <String> entity = new HttpEntity<String>(headers);
+	      
+	      return restTemplate.exchange("http://localhost:8080/service-patient", HttpMethod.GET, entity, String.class).getBody();
+	   }
+
+	@GetMapping("/doctors")
+	public List<Doctor> findAll() {
+
+		return doctorService.findAll();
+
+	}
+
 	@GetMapping("doctors/{doctorId}")
 	public Doctor getDoctor(@PathVariable Long id) {
-		
+
 		Doctor d = doctorService.findById(id);
-		
+
 		if (d == null) {
 			throw new RuntimeException("Doctor id not found - " + id);
 		}
-		
+
 		return d;
 	}
 
 	@PostMapping("doctors/addDoctor")
-	public  Doctor addDoctor(@RequestBody Doctor d) {
-	
+	public Doctor addDoctor(@RequestBody Doctor d) {
+
 		doctorService.save(d);
 		return d;
 	}
-	
-		
+
 	@PutMapping("doctors/updatedoctor")
 	public Doctor updateDoctor(@RequestBody Doctor d) {
-		
+
 		doctorService.save(d);
-		
+
 		return d;
 	}
 
 	@DeleteMapping("/doctors/{doctorId}")
 	public String deleteDoctor(@PathVariable Long id) {
-		
+
 		Doctor p = doctorService.findById(id);
-		
+
 		if (p == null) {
 			throw new RuntimeException("the doctor id not found - " + id);
 		}
-		
+
 		doctorService.deleteById(id);
-		
+
 		return "Deleted doctor id - " + id;
 	}
 
